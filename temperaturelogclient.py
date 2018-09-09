4af43e1db173371e42761187e8b325e291eb6261
ROBrownsmith <robrownsmith@hotmail.com>
2018-09-09 14:31:29 +0000

    First commit

---
+++ temperaturelogclient.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+# Import Libraries
+import os
+import glob
+import json 
+import requests
+import time
+import datetime
+import schedule
+#from flask import jsonify
+
+def postjsonviahttp():
+  # Initialize the GPIO Pins
+  os.system('modprobe w1-gpio') # Turns on the GPIO module 
+  os.system('modprobe w1-therm') # Turns on the Temperature module
+   
+  # Finds the correct device file that holds the temperature data
+  base_dir = '/sys/bus/w1/devices/' 
+  device_folder = glob.glob(base_dir + '28*')[0] 
+  device_file =device_folder + '/w1_slave'
+  devicename_file =device_folder + '/name' 
+  # A function that reads the sensors data
+  def read_temp_raw():
+    f = open(device_file, 'r') # Opens the temperature device file
+    lines = f.readlines() # Returns the text
+    f.close()
+    return lines
+
+  def read_name():
+    d = open(devicename_file, 'r')
+    devicename = d.read()
+    devicename = devicename.replace("\n","")
+    d.close()
+    return devicename 
+
+  # Convert the value of the sensor into a temperature
+  def read_temp():
+    lines = read_temp_raw() # Read the temperature 'device file'
+   
+    # While the first line does not contain 'YES', wait for 0.2s and then read the device file 
+    # again.
+    while lines[0].strip()[-3:] != 'YES':
+      time.sleep(0.2)
+      lines = read_temp_raw()
+   
+    # Look for the position of the '=' in the second line of the device file.
+    equals_pos = lines[1].find('t=')
+   
+    # If the '=' is found, convert the rest of the line after the '=' into degrees Celsius
+    if equals_pos != -1:
+      temp_string = lines[1][equals_pos+2:]
+      temp_c = float(temp_string) / 1000.0
+      return temp_c
+   
+  timestamp = datetime.datetime.utcnow()
+  serialnum = read_name()
+  tempc = read_temp()
+
+
+  ##create a dict for dumping to JSON
+  data = {}
+  data["Timestamp"] = timestamp.__str__()
+  data["Serialnumber"] = serialnum
+  data["Temperature"] = tempc
+  senddata = json.dumps(data)
+  print (senddata)
+#post JSON to server
+  url = "http://192.168.0.4:5000/insertTemp"
+  headers = {'content-type': 'application/json'}
+  r = requests.post(url, data=senddata, headers=headers)
+  return r
+#schedule
+schedule.every(1).minutes.do(postjsonviahttp)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
